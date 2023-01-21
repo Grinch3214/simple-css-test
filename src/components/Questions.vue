11e1762c499d036b3f87a79d13a269fca8d7a919
Max <scorpiomax90@gmail.com>
2023-01-21 12:04:28 +0200

    adding start template

---
+++ src/components/Questions.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="quiz__questions">
+      <div class="quiz__progress">
+        <div class="quiz__bar"></div>
+        <div class="quiz__status">1 out of 3 questions answered</div>
+      </div>
+      <div class="quiz__wrap">
+        <div class="quiz__question">Sample Question 1</div>
+        <ul class="quiz__answers">
+          <li class="quiz__answer">Sample Answer 1</li>
+          <li class="quiz__answer">Sample Answer 2</li>
+          <li class="quiz__answer">Sample Answer 3</li>
+          <li class="quiz__answer">Sample Answer 4</li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+	name: 'Questions'
+}
+</script>
---
+++ src/components/Result.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="quiz__result">
+      <div class="quiz__title">You got sample result 1!</div>
+      <div class="quiz__desc">Enter a short description here about the result.</div>
+    </div>
+</template>
+
+<script>
+export default {
+	name: 'Result'
+}
+</script>
